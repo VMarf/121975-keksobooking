f9deea391855a02ba655da4eccae10fe3ec7cb31
Marfin Vladislav <marfinvlad@gmail.com>
2017-09-05 13:56:19 +0300

    completed save function

--- js/backend.js
+++ js/backend.js
@@ -1,3 +1,6 @@
+// @TODO: Что-то не так с параметрами функции load и save
+// @TODO: Функции load и save практически идентичны, вынести дублирующий код в отдельную функцию
+
 'use strict';
 
 (function () {
@@ -5,8 +8,14 @@
   // Адрес, с которого получаем данные
   var GET_URL = 'https://1510.dump.academy/keksobooking/data';
 
+  // Адрес, на который отправляем данные
+  var POST_URL = 'https://1510.dump.academy/keksobooking';
+
   var onLoad = function (data) {
     var ads = data;
+
+    // @TODO: Удалить
+    console.log(ads);
   };
 
   var onError = function (message) {
@@ -18,7 +27,7 @@
     var xhr = new XMLHttpRequest();
 
     xhr.responseType = 'json';
-    xhr.timeout = 1000;
+    xhr.timeout = 2000;
 
     xhr.addEventListener('load', function () {
       var error;
@@ -59,7 +68,46 @@
 
   // Для отправки данных из формы на сервер
   var save = function (data, funcLoad, funcError) {
+    var xhr = new XMLHttpRequest();
 
+    xhr.responseType = 'json';
+    xhr.timeout = 2000;
+
+    xhr.addEventListener('load', function () {
+      var error;
+
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+        default:
+          error = 'Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText;
+      }
+
+      if (error) {
+        onError(error);
+      }
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.open('POST', POST_URL);
+    xhr.send(data);
   };
 
   window.backend = {
@@ -68,4 +116,4 @@
   };
 })();
 
-// @TODO: Что-то не так с параметрами функции load
+window.backend.load();
