d1bec5b4f1f83905e997ff3564a2cb00fc461355
Marfin Vladislav <marfinvlad@gmail.com>
2017-09-05 16:09:47 +0300

    completed backend module

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,3 @@
-// @TODO: Что-то не так с параметрами функции load и save
-// @TODO: Функции load и save практически идентичны, вынести дублирующий код в отдельную функцию
-
 'use strict';
 
 (function () {
@@ -11,19 +8,24 @@
   // Адрес, на который отправляем данные
   var POST_URL = 'https://1510.dump.academy/keksobooking';
 
-  var onLoad = function (data) {
-    var ads = data;
+  var requestError = function (message) {
+    var errorTooltip = document.createElement('div');
+    var errorTooltipText = document.createElement('span');
 
-    // @TODO: Удалить
-    console.log(ads);
+    errorTooltip.appendChild(errorTooltipText);
+
+    errorTooltip.classList.add('request-error');
+    errorTooltipText.classList.add('request-error__text');
+    errorTooltipText.textContent = message;
+
+    document.body.insertAdjacentElement('afterbegin', errorTooltip);
+
+    setTimeout(function () {
+      errorTooltip.remove();
+    }, 3000);
   };
 
-  var onError = function (message) {
-    console.error(message);
-  };
-
-  // Для получения данных с сервера
-  var load = function (funcLoad, funcError) {
+  var sendRequest = function (method, url, onLoad, onError, data) {
     var xhr = new XMLHttpRequest();
 
     xhr.responseType = 'json';
@@ -62,58 +64,23 @@
       onError('Произошла ошибка соединения');
     });
 
-    xhr.open('GET', GET_URL);
-    xhr.send();
-  };
-
-  // Для отправки данных из формы на сервер
-  var save = function (data, funcLoad, funcError) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.responseType = 'json';
-    xhr.timeout = 2000;
-
-    xhr.addEventListener('load', function () {
-      var error;
-
-      switch (xhr.status) {
-        case 200:
-          onLoad(xhr.response);
-          break;
-        case 400:
-          error = 'Неверный запрос';
-          break;
-        case 401:
-          error = 'Пользователь не авторизован';
-          break;
-        case 404:
-          error = 'Ничего не найдено';
-          break;
-        default:
-          error = 'Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText;
-      }
-
-      if (error) {
-        onError(error);
-      }
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.open('POST', POST_URL);
+    xhr.open(method, url);
     xhr.send(data);
   };
 
+  // Для получения данных с сервера
+  var load = function (onLoad, onError) {
+    sendRequest('GET', GET_URL, onLoad, onError);
+  };
+
+  // Для отправки данных на сервер
+  var save = function (onLoad, onError, data) {
+    sendRequest('POST', POST_URL, onLoad, onError, data);
+  };
+
   window.backend = {
+    requestError: requestError,
     load: load,
     save: save
   };
 })();
-
-window.backend.load();
